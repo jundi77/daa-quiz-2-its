97ea36b0ed25d694ad31aa14e51bf1bd86049367
Jundullah H. R <jundihanif77@gmail.com>
2021-06-07 01:02:11 +0700

    feat: add NQueenSolver class and it's example
    make it easy :)

---
+++ js/NQueenSolverExample.js
@@ -0,0 +1,18 @@
+/**
+ * Contoh cara panggil kelas NQueenSolver
+ */
+
+import NQueenSolver from './NQueenSolver.js'
+let test = new NQueenSolver([[]]); // !remember selalu array 2D
+
+/**
+ * Available methods:
+ * 
+ * - constructor (ofcourse :) )
+ * - search
+ * - stop
+ * - getFinishedNode
+ * 
+ * ! Highly recommended memakai syntax autocomplete untuk
+ * ! memudahkan dirimu :)
+ */
